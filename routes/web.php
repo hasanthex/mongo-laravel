db86e12fa472d9b98ce65ce65fb3403a52e913e6
hasanthex <hasan18353@gmail.com>
2021-07-04 17:42:02 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/book', [App\Http\Controllers\BookController::class, 'index'])->name('item');
